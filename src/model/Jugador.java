ee69d0ad96b99423047f74fbcaf0036b7cd0b3ee
enior <psantosdemora@gmail.com>
2025-03-04 13:56:13 +0100

    opcion ayuda en el menu

--- src/model/Jugador.java
+++ src/model/Jugador.java
@@ -1,8 +1,52 @@
 package model;
+import java.util.Scanner;
 
 public class Jugador {
 
-	
+	 // Atributos
+	   long puntuacion; // int + más largo
+	   String nombre;
+	   int racha;
+	   int puntos;
+	   
+	   
+	   
+	   
+	   String respuesta;
+	   static Scanner sc = new Scanner(System.in);
+	   // constructor vacío
+	   public Jugador() {
+	   }
+	   // constructor lleno
+	   public Jugador(long puntuacion, int puntos, String respuesta) {
+	       this.puntuacion = puntuacion;
+	       this.puntos = puntos;
+	       this.respuesta = respuesta;
+	   }
+	   // Getters y Setters
+	   // método para obtener el valor del atributo. Lee el valor del atributo desde fuera de la clase sin ser directamente accesible
+	   public long getPuntuacion() {
+	       return puntuacion; // no requiere parámetros, devuelve el valor del atributo de la clase. No necesita información adicional
+	   }
+	   // método para modificar el valor del atributo. Requiere parámetro porque el propósito es recibir el nuevo valor del atributo.
+	   public void setPuntuacion(long puntuacion) {
+	       this.puntuacion = puntuacion; /*El parámetro (long puntuacion) es necesario. Setter necesita saber qué valor asignar a la
+	       							variable.*/
+	   }
+	   public int getPuntos() {
+	       return puntos;
+	   }
+	   public void setPuntos(int puntos) {
+	       this.puntos = puntos; // this hace referencia a la variable de la clase
+	   }
+	   public String getRespuesta() {
+	       return respuesta;
+	   }
+	   public void setRespuesta(String respuesta) {
+	       this.respuesta = respuesta;
+	   }
+	  
+	}
 
 
 
@@ -20,6 +64,4 @@ public class Jugador {
 
 
 
-
-}
 
--- src/model/Trivia.java
+++ src/model/Trivia.java
@@ -2,6 +2,7 @@ package model;
 
 import java.util.Random;
 import java.util.Scanner;
+import java.util.Scanner;
 
 public class Trivia {
 	
@@ -117,5 +118,113 @@ public class Trivia {
 		
 		return puntuacion;
 	}
+	
+	 public void menu() {
+		  Scanner sc = new Scanner(System.in);
+		  String decision;
+		  
+	      System.out.println(Color.YELLOW_BOLD + "                   BIENVENIDO AL RELOJ DE ARENA"  + Color.YELLOW_BOLD_BRIGHT + "\n1.Iniciar Juego  |  2.¿Que es Reloj de Arena?  |  3.Salir\n");
+	      decision = sc.nextLine();
+	      while(!decision.equals("1") && !decision.equals("2") && !decision.equals("3")) {
+	    	  System.out.println(Color.RED_BRIGHT + "Visto que ha respondido incorrectamente le recordamos que debe introducir" + Color.RED_BOLD_BRIGHT + " 1, 2 o 3");
+	    	  decision = sc.nextLine();
+	      }
+	      
+	      switch(decision) {
+	      
+		      case "1":
+		    	 
+		    	  break;
+		      case "2":
+			    	ayuda(); 
+			      break;
+		      case "3":
+			    	 
+		    	  break;
+	      }
+	   }
+	 public void ayuda() {
+		 Scanner sc = new Scanner(System.in);
+		 String decision = "Si";
+		 
+		 while(decision.equalsIgnoreCase("Si")) {
+			 System.out.println(Color.GREEN_BOLD_BRIGHT + "Bienvenido, en que te puedo ayudar hoy:\n" + Color.RESET + " 1.Modos de juego" + " 2.Menus" + " 3.Programacion" + " 4.Manual completo" + " 5.Salir");
+			 decision = sc.nextLine();
+			 
+			 while(!decision.equals("1") && !decision.equals("2") && !decision.equals("3") && !decision.equals("4") && !decision.equals("5")) { 
+				 System.out.println("Veo que necesitas ayuda con el funcionamiento de los menus, ¿Es eso cierto?  (Tienes responder con Si o No)");
+				 decision = sc.nextLine();
+				 if(decision.equalsIgnoreCase("Si")) decision = "2";
+				 else if(decision.equalsIgnoreCase("No")) {
+					 System.out.println("Bienvenido, en que te puedo ayudar hoy:\n" + " 1.Modos de juego" + " 2.Menus" +  " 3.Programacion" + " 4.Manual completo" + " 5.Salir");
+					 decision = sc.nextLine();
+				 }
+			 }
+			 
+			 switch(decision) {
+			 
+			 case "1":
+				 System.out.println(Color.GREEN_BOLD_BRIGHT + "De acuerdo, elige sobre que modo quieres ayuda: \n" + Color.RESET + "1.Juego individual 2.Juego Multijugador 3.Manual completo 4.Salir");
+				 decision = sc.nextLine();
+				 
+				 while(!decision.equals("1") && !decision.equals("2") && !decision.equals("3") && !decision.equals("4")) { 
+					 System.out.println(Color.RED_BOLD_BRIGHT + "Tal vez deberias revisar la ayuda de los menus, pero si no intenta introducir 1,2 o 3" + Color.RESET);
+					 System.out.println("1.Juego individual 2.Juego Multijugador 3.Manual completo 4.Salir");
+					 decision = sc.nextLine();
+				 }
+				 switch(decision) {
+				 case "1":
+					 System.out.println(Color.WHITE_BOLD_BRIGHT + "\nEl modo individual consiste en un juego clasico de preguntas y respuestas,\ndonde comienzas con tres vidas y tu objetivo es responder el mayor numero de preguntas." + Color.RESET);
+					 break;
+				 case "2":
+					 System.out.println(Color.WHITE_BOLD_BRIGHT + "\nEl modo multijugador consiste en un juego clasico de preguntas y respuestas,\ndonde debes competir con los otros jugadores para ver quien responde mas rapido o tiene una mayor racha de preguntas" + Color.RESET);
+					 break;
+				 case "3":
+					 System.out.println(Color.WHITE_BOLD_BRIGHT + "MANUAL DE MODOS DE JUEGO");
+					 System.out.println(Color.WHITE_BOLD_BRIGHT + "\n1.El modo individual consiste en un juego clasico de preguntas y respuestas,\ndonde comienzas con tres vidas y tu objetivo es responder el mayor numero de preguntas." + Color.RESET);
+					 System.out.println(Color.WHITE_BOLD_BRIGHT + "\n2.El modo multijugador consiste en un juego clasico de preguntas y respuestas,\ndonde debes competir con los otros jugadores para ver quien responde mas rapido o tiene una mayor racha de preguntas" + Color.RESET);
+					 break;
+				 case "4":
+					 decision = "salir";
+					 break;
+				 }
+				 break;
+			 case "2":
+				 System.out.println(Color.WHITE_BOLD_BRIGHT + "MANUAL DE MENUS");
+				 System.out.println(Color.WHITE_BOLD_BRIGHT + "\nLos menus suelen ser: \n1.Elecciones por numero, un ejemplo como 1.A 2.B, "
+				 					+ "donde debes introducir uno de los numeros que aparecen en el selector en este caso 1 o 2. "
+				 					+ "\n2.Elecciones por texto, como Si o No, donde debes introducir una respuesta que aparezca tras la pregunta en este caso Si o No. "
+				 					+ "\n\n-Sumado a esto el programa les ayudara si cometen algun error o les recomendara soluciones para ello" + Color.RESET);
+				 break;
+			 case "3":
+				 System.out.println("");
+				 break;
+			 case "4":
+				 System.out.println("MANUAL COMPLETO DE RELOJ DE ARENA");
+				 
+				 System.out.println(Color.WHITE_BOLD_BRIGHT + "MANUAL DE MODOS DE JUEGO");
+				 System.out.println(Color.WHITE_BOLD_BRIGHT + "\n1.El modo individual consiste en un juego clasico de preguntas y respuestas,\ndonde comienzas con tres vidas y tu objetivo es responder el mayor numero de preguntas." + Color.RESET);
+				 System.out.println(Color.WHITE_BOLD_BRIGHT + "\n2.El modo multijugador consiste en un juego clasico de preguntas y respuestas,\ndonde debes competir con los otros jugadores para ver quien responde mas rapido o tiene una mayor racha de preguntas" + Color.RESET);
+				 
+				 System.out.println(Color.WHITE_BOLD_BRIGHT + "MANUAL DE MENUS");
+				 System.out.println(Color.WHITE_BOLD_BRIGHT + "\nLos menus suelen ser: \n1.Elecciones por numero, un ejemplo como 1.A 2.B, "
+				 					+ "donde debes introducir uno de los numeros que aparecen en el selector en este caso 1 o 2. "
+				 					+ "\n2.Elecciones por texto, como Si o No, donde debes introducir una respuesta que aparezca tras la pregunta en este caso Si o No. "
+				 					+ "\n\n-Sumado a esto el programa les ayudara si cometen algun error o les recomendara soluciones para ello" + Color.RESET);
+				
+				 break;
+			 }
+			 
+			 System.out.println("\nTras esto tienes alguna duda mas: Si o No");
+			 decision = sc.nextLine();
+			 
+			 while(!decision.equalsIgnoreCase("Si") && !decision.equalsIgnoreCase("No")) { 
+				 System.out.println("\nTal vez deberias leer de nuevo el manual de menus");
+				 System.out.println("\nTras esto tienes alguna duda mas: Si o No");
+				 decision = sc.nextLine();
+			 }
+		 }
+		 
+	 }
 
 }
